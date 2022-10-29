519e30dc191184249f0223e39eabb5354de5808a
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-29 17:06:22 +0530

    (Shubham)Add: Flipping coin and calculating percentage of heads and tails

---
+++ Day5BasicPrograms/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Day5BasicPrograms
+{
+    public class FlipCoin
+    {
+        
+        int count_Head = 0, count_Tail = 0;
+        public void Flip()
+        {
+
+            Console.WriteLine("Enter the number of times you want to flip Coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                double check = random.NextDouble();
+                if (check > 0.5)
+                    count_Head++;
+                else
+                    count_Tail++;
+            }
+            Console.WriteLine("Head comes {0} times ", count_Head);
+            Console.WriteLine("Tail comes {0} times ", count_Tail);
+            float headPercentage = ((float)count_Head / (float)num) * 100;
+            Console.WriteLine("Head percentage is {0} ", headPercentage);
+            float tailPercentage = ((float)count_Tail / (float)num) * 100;
+            Console.WriteLine("Tail percentage is {0} ", tailPercentage);
+
+        }
+    }
+}
+
+    
+
--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Day5BasicPrograms;
+using System;
 namespace Day5BasicProgrmas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the assignment part");
+            Console.WriteLine("Welcome to the Flip coin program");
+            FlipCoin percentage=new FlipCoin();
+            percentage.Flip();
         }
     }
 }
