d0d0a60636be14705e61645a2655de0cb935e13c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-29 16:45:07 +0530

    (Shubham)Add: Project folder structure created

---
+++ Day5BasicPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Day5BasicProgrmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the assignment part");
+        }
+    }
+}
