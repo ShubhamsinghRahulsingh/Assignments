f64776ee1686caca242c416bb9bf67d33cb13ec6
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-30 00:09:52 +0530

    (Shubham)Add: Prints table of power of two

---
+++ Day5BasicPrograms/PowerOfTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Day5BasicPrograms
+{
+    public class PowerOfTwo
+    {
+       public void Table ()
+        {
+            Console.WriteLine("Enter number between 0 to 30 upto which you want table");
+            int number=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=number;i++)
+            {
+                int multiply = 2 * i;
+                Console.WriteLine("2*{0}= {1} ", i,multiply);
+
+            }
+        }
+
+    }
+}
--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -6,19 +6,30 @@ namespace Day5BasicProgrmas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.FlipCoin" +" \n" + "2.LeapYear");
+            bool flag = true;
+            while(flag)
+            { 
+            Console.WriteLine("1.FlipCoin" +" \n" + "2.LeapYear" + "\n" +"3.PowerOfTwo" + "\n" +"4.EXIT");
             Console.WriteLine("Enter the option from the above that you want to go for");
             int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    FlipCoin percentage = new FlipCoin();
-                    percentage.Flip();
-                    break;
-                case 2:
-                    LeapYear leap=new LeapYear();
-                    leap.Leap();
-                    break;
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin percentage = new FlipCoin();
+                        percentage.Flip();
+                        break;
+                    case 2:
+                        LeapYear leap = new LeapYear();
+                        leap.Leap();
+                        break;
+                    case 3:
+                        PowerOfTwo table = new PowerOfTwo();
+                        table.Table();
+                        break;
+                    case 4:
+                        flag= false;
+                        break;
+                }
         }
         }    
     }
