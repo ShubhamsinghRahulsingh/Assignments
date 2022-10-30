37275d452f4bcc9ff6ff60496f77b608cfe0cf00
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-30 19:14:36 +0530

    (Shubham)Add: check whether a entered alphabet is vowel or consonant

---
+++ Day5BasicPrograms/VowelConsonant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Day5BasicPrograms
+{
+    public class VowelConsonant
+    {
+        public void Vowel()
+        {
+            Console.WriteLine("Enter the alphabet you want to check");
+            char ch= Convert.ToChar(Console.ReadLine().ToLower());
+            switch(ch)
+            {
+                case 'a':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'e':
+                    Console.WriteLine("Entered alphabet is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Entered alphabet is a consonant");
+                    break;
+            }
+        }
+    }
+}
